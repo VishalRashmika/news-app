5b1efa1ddeb2a3233e8fe2af34652ba71271aefc
VishalRashmika <vishal.rashmika.perera@gmail.com>
2024-07-04 20:39:26 +0530

    news feed api

---
+++ news-app/pages/feed/[pageid].js
@@ -0,0 +1,26 @@
+export const Feed = ({ pageNumber, articles}) =>{
+    return (<>Hello World</>)
+};
+
+export const getServerSideProps = async pageContext =>{
+    const pageNumber = pageContext.query.pageid;
+
+    if  (!pageNumber || pageNumber < 1 || pageNumber > 5){
+        return{
+            props: {
+                articles: [],
+                pageNumber: 1,
+            }
+        }
+    }
+}
+const apiResponse = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=us&pageSize=5&page=${pageNumber}`,
+    {
+        headers: {
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`,
+        },
+    },
+);
+
+export default Feed;
