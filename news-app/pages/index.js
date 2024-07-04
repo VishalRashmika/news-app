d9bbc208f8e21ddd27db6191fdc69db230965fbe
VishalRashmika <vishal.rashmika.perera@gmail.com>
2024-07-04 20:18:37 +0530

    new toolbar

--- news-app/pages/index.js
+++ news-app/pages/index.js
@@ -2,9 +2,21 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { Toolbar } from "@/components/toolbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (<>Hello World!!!</>)
+  return (
+    <div className="page-container">
+
+    <Toolbar/>
+
+      <div className={styles.main}>
+        <h1>Next.js New App</h1>
+
+        <h3>Come here to see the latest news articles</h3>
+      </div>
+    </div>
+  );
 }
